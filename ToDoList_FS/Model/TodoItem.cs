a8b35f5361f2b36a4ddb8e2bd9426d0dd2c3ebc2
Luan Pham <luan.pham@tscmiami.com>
2025-05-21 09:16:16 +0700

    update code

--- ToDoList_FS/Model/TodoItem.cs
+++ ToDoList_FS/Model/TodoItem.cs
@@ -20,22 +20,22 @@ namespace ToDoList_FS.Model
         public string? Id { get; set; }
 
         [BsonElement("title")]
-        public string Title { get; set; }
+        public string? Title { get; set; }
 
         [BsonElement("description")]
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
         [BsonElement("status")]
         public int Status { get; set; }
 
         [BsonElement("fromDate")]
-        public string FromDate { get; set; }
+        public string? FromDate { get; set; }
 
         [BsonElement("toDate")]
-        public string ToDate { get; set; }
+        public string? ToDate { get; set; }
 
         [BsonElement("userId")]
-        public string UserId { get; set; }
+        public string? UserId { get; set; }
     }
 
     // Model cho GET/Response (không cần [Required])
@@ -44,33 +44,33 @@ namespace ToDoList_FS.Model
         [BsonId]
         public string? Id { get; set; }
         [BsonElement("title")]
-        public string Title { get; set; }
+        public string? Title { get; set; }
         [BsonElement("description")]
-        public string Description { get; set; }
+        public string? Description { get; set; }
         [BsonElement("status")]
         public int Status { get; set; }
         [BsonElement("fromDate")]
-        public string FromDate { get; set; }
+        public string? FromDate { get; set; }
         [BsonElement("toDate")]
-        public string ToDate { get; set; }
+        public string? ToDate { get; set; }
         [BsonElement("userId")]
-        public string UserId { get; set; }
+        public string? UserId { get; set; }
     }
 
     // Model cho POST/PUT (có [Required])
     public class TodoItemRequest
     {
         [Required]
-        public string Title { get; set; }
+        public string? Title { get; set; }
         [Required]
-        public string Description { get; set; }
+        public string? Description { get; set; }
         [Required]
         public int Status { get; set; }
         [Required]
-        public string FromDate { get; set; }
+        public string? FromDate { get; set; }
         [Required]
-        public string ToDate { get; set; }
+        public string? ToDate { get; set; }
         [Required]
-        public string UserId { get; set; }
+        public string? UserId { get; set; }
     }
 }
